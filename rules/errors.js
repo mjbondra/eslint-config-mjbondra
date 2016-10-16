82b03ad322e95b5f642c8adbf8fa0bdcf663b53f
Michael J. Bondra <mjbondra@gmail.com>
2016-10-15 17:18:04 -0700

    update possible errors rules

--- rules/errors.js
+++ rules/errors.js
@@ -10,12 +10,18 @@ module.exports = {
     'no-empty': ['error', { allowEmptyCatch: true }],
 
     // disallow unnecessary parentheses around expressions
-    'no-extra-parens': ['error', 'all'],
+    'no-extra-parens': ['error', 'all', { conditionalAssign: false }],
 
     // disallow unnecessary semicolons
     'no-extra-semi': 'error',
 
+    // disallow function or var declarations in nested blocks
+    'no-inner-declarations': ['error', 'both'],
+
     // disallow use of Object.prototypes builtins directly
-    'no-prototype-builtins': 'error'
+    'no-prototype-builtins': 'error',
+
+    // enforce comparing typeof expressions against valid strings
+    'valid-typeof': ['error', { requireStringLiterals: true }]
   }
 };
